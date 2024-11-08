b9422f19a15a887fb9fed8d0cb8565b17ab8a8d0
Felipe Henrique <151560338+LipeOzyy@users.noreply.github.com>
2024-11-08 10:32:21 -0300

    controlador_1.2

--- controlador.py
+++ controlador.py
@@ -1,3 +1,20 @@
 import socket
 
-def inciar_servidor(server_ip, server_pot):
+def inciar_servidor(server_ip, server_port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_socket.bind((server_ip, server_port))
+
+    server_socket.listen(1) #aceita até uma conexão
+    print(f"Conexão de {server_ip}:{server_port}")
+
+    client_socket, client_address = server_socket.accept()
+    print(f"Conexão estabelecida {client_address}")
+
+    while True: 
+        command = input("Shell>")
+
+        if command.lower() == "exit":
+            client_socket.send("exit".encode())
+            break
+        
